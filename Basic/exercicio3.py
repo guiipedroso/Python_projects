7b1d9d8b86d983c225cf6029a40e410430fc0f90
Guilherme Pedroso <guilhermepedrosogg@gmail.com>
2024-01-14 01:44:04 -0300

    String

--- Basic/exercicio3.py
+++ Basic/exercicio3.py
@@ -1,7 +1,8 @@
 name = "Guilherme"
+last_name = "Pedroso"
 age = 21
 money = 291178
 
-print("User: "+name[0:3])
+print("User: "+name[0:3]+" "+last_name)
 
 print("Name: {}, Age: {} and Account R${}".format(name, age, money)) 
